359f99674a610856d36d1f9f861aac363e1c072d
Muhammad Shahrukh <>
2024-07-29 12:30:37 +0500

    COMCL-635: Upgrade civix

--- amazonsns.php
+++ amazonsns.php
@@ -22,48 +22,12 @@ function amazonsns_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function amazonsns_civicrm_postInstall() {
-  _amazonsns_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function amazonsns_civicrm_uninstall() {
-  _amazonsns_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function amazonsns_civicrm_enable() {
   _amazonsns_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function amazonsns_civicrm_disable() {
-  _amazonsns_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function amazonsns_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _amazonsns_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
